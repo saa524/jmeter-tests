5a505d6da4406fdc9d5cea55260097857eec2dd1
saa524 <svetlana.angelova@informed.com>
2016-11-04 16:10:41 +0000

    Add comments to download script

--- download_jmeter.sh
+++ download_jmeter.sh
@@ -1,3 +1,12 @@
+# All operations are performed in the Jenkins workspace
+# Get the latest JMeter binary as a zip
 wget -o apache-jmeter-3.0.zip http://mirror.ox.ac.uk/sites/rsync.apache.org//jmeter/binaries/apache-jmeter-3.0.zip
+
+# Combine all the zip files, in case it downloads in multipart files
 cat apache-jmeter-3.0.zip* > apache-jmeter-3.0.zip
+
+# Unzip the newly created zip file
 unzip apache-jmeter-3.0.zip
+
+# Clean up
+rm -f apache-jmeter-3.0.zip*
