cc6452e29374b489fe5b0baef7288bc51ccb3052
saa524 <svetlana.angelova@informed.com>
2016-11-04 12:10:35 +0000

    Add script to install jmeter

--- download_jmeter.sh
+++ download_jmeter.sh
@@ -1,4 +1,3 @@
 wget -o apache-jmeter-3.0.zip http://mirror.ox.ac.uk/sites/rsync.apache.org//jmeter/binaries/apache-jmeter-3.0.zip
 cat apache-jmeter-3.0.zip* > apache-jmeter-3.0.zip
 unzip apache-jmeter-3.0.zip
-cd apache-jmeter-3.0
